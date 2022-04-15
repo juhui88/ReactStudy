522fadcd22bc72115f1d7606d9b8d119f9f9fc32
unknown <sand8594@hufs.ac.kr>
2022-04-15 17:19:31 +0900

    함수형 컴포넌트

---
+++ component/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = '리액트';
+  return (
+    <div className = "react">{name}</div>
+  );
+}
+
+export default App;
