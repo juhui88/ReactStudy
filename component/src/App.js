e265d6c28b0eadd8a7c8738a75dfbaeb8ff44cfe
unknown <sand8594@hufs.ac.kr>
2022-04-18 19:58:08 +0900

    event handling

--- component/src/App.js
+++ component/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import MyComponent from './MyComponent';
 import Counter from './Counter';
 import Say from './Say';
-
+import EventPractice from './EventPractice'
 const App = () => {
-  return <Say/>
+  return (
+  <EventPractice/>
+  );
 }
 
 export default App;
