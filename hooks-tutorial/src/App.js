86476ceee869db8c194b49f0c011df659983eeeb
unknown <sand8594@hufs.ac.kr>
2022-05-31 21:11:20 +0900

    useEffect

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Counter from './Counter';
 import Info from './Info';
 
 const App = () => {
+  const [visible, setVisible] = useState(false);
   return (
-    <Info/>
+    <div>
+      <button 
+        onClick = {() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr/>
+      {visible && <Info/>}
+    </div>
   )
 
 };
