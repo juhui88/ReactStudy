4bf0e154c2051df86457d26ca9401f033fc587d3
unknown <sand8594@hufs.ac.kr>
2022-06-05 13:54:36 +0900

    useMemo

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
 import Counter from './Counter';
 import Info from './Info';
+import Average from './Average';
 
 const App = () => {
   
-  return (
-    <Info/>
-  )
+  return <Average/>
 
 };
 
