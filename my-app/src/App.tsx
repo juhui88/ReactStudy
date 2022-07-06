553335aa25b7d48fd5530fdbee88ba54092604a6
unknown <sand8594@hufs.ac.kr>
2022-07-06 21:03:15 +0900

    Typescript Theme

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,16 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+  
+`
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`
 
 function App() {
-  const [value, setValue] = useState(""); 
+  /*const [value, setValue] = useState(""); 
   // 처음 String형태 였으므로 이후에도 계속 String 이어야 오류가 안 남
 
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
@@ -13,19 +21,11 @@ function App() {
   const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("hello", value);
-  }
+  }*/
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input 
-          type = "text" 
-          placeholder='username'
-          onChange={onChange}
-          value = {value}
-          />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
