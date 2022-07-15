9debb642b7ef059a31741427b38403c0b4eeb472
unknown <sand8594@hufs.ac.kr>
2022-07-15 20:25:15 +0900

    after react-hook-form

--- state-management/src/ToDoList.tsx
+++ state-management/src/ToDoList.tsx
@@ -2,27 +2,15 @@ import React, { useState } from "react";
 import {useForm} from 'react-hook-form';
 
 function TodoList() {
-     const [toDo, setToDo] = useState("");
-    const [toDoError, setToDoError] = useState("")
-    const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-        const {currentTarget: {value}} = e;
-        setToDoError("");
-        setToDo(value);
+    const {register, watch, handleSubmit} = useForm();
+    const onValid = (data:any) => {
+        console.log(data)
     }
-    const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if(toDo.length < 10){
-            return setToDoError("To do should be longer")
-        }
-        console.log("sumbit")
-    } 
-
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input onChange = {onChange}placeholder="Write a to do" />
+            <form onSubmit={handleSubmit(onValid)}>
+                <input {...register("toDo", {required:true, minLength: 10})} placeholder="Write a to do" />
                 <button>Add</button>
-                {toDoError!=="" ? toDoError: null }
             </form>
         </div>
     )
