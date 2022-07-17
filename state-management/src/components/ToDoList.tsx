db45b8e2c82b268ac6c79e2c168d3fb0c6105959
unknown <sand8594@hufs.ac.kr>
2022-07-17 16:12:47 +0900

    selectors 이용

--- state-management/src/components/ToDoList.tsx
+++ state-management/src/components/ToDoList.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-import {toDoState} from './atom';
+import { useRecoilState, useRecoilValue } from "recoil";
+import {categoryState, toDoSelector, toDoState} from './atom';
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function TodoList() {
-    const toDos = useRecoilValue(toDoState)
-    console.log(toDos)
+    const [category, setCategory] = useRecoilState(categoryState);
+    const toDos = useRecoilValue(toDoSelector);
+    
+    const onInput = (e:React.FormEvent<HTMLSelectElement>) => {
+        const {currentTarget: {value}} = e
+        setCategory(value)
+    }
     return (
         <div>
             <h1>To Dos</h1>
             <hr/>
+            <form>
+                <select value = {category} onInput={onInput}>
+                    <option value="TO_DO">To Do</option>
+                    <option value="DOING">Doing</option>
+                    <option value="DONE">Done</option>
+                </select>
+            </form>
             <CreateToDo/>
-            <ul>
-                {toDos.map(toDo => <ToDo key = {toDo.id} {...toDo}/>)}
-            </ul>
+            {toDos?.map(toDo => <ToDo key = {toDo.id} {...toDo}/> )}
         </div>
     )
 }
--- state-management/src/components/atom.tsx
+++ state-management/src/components/atom.tsx
@@ -1,11 +1,27 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
     text: string;
     id: number;
     category: "DONE" | "DOING" | "TO_DO";
 }
+
+export const categoryState = atom({
+    key: "category",
+    default: "TO_DO"
+})
+
 export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
 });
+
+export const toDoSelector = selector({
+    key: "toDoSelector",
+    get: ({get}) => {
+        const toDos = get(toDoState)
+        const category = get(categoryState)
+
+        return toDos.filter(todo => todo.category === category)
+    }
+})
