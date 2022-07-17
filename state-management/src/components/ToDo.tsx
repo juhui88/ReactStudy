aa5d6dbb577bdfe3d9828fe152361d8d45ce4126
unknown <sand8594@hufs.ac.kr>
2022-07-17 15:03:28 +0900

    todo category 관리

--- state-management/src/components/ToDo.tsx
+++ state-management/src/components/ToDo.tsx
@@ -1,13 +1,47 @@
-import { ITodo } from "./atom";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "./atom";
 
-function ToDo({text, category}:ITodo) {
-    
+function ToDo({text, category, id}:IToDo) {
+    const setToDos = useSetRecoilState(toDoState);
+    const onClick = (e:React.MouseEvent<HTMLButtonElement>) => {
+        const {currentTarget: {name}, } = e
+        console.log(name)
+        setToDos((prev) => prev.map((oldToDo) => {
+            if(oldToDo.id === id) {
+                return {text, id, category: name as any};
+            }
+            return oldToDo;
+        }))
+        /* setToDos((oldToDos) => {
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id === id)
+            console.log(targetIndex)
+            const newToDo = { text, id, category: name as any};
+            return [
+                ...oldToDos.slice(0, targetIndex),
+                newToDo,
+                ...oldToDos.slice(targetIndex + 1),
+              ];
+        }) */
+    };
     return (
         <li>
             <span>{text}</span>
-            <button>Done</button>
-            <button>Doing</button>
-            <button>To Do</button>
+            {category !== "DOING" && (
+                <button name="DOING" onClick={onClick}>
+                    Doing
+                </button>
+            )}
+            {category !== "TO_DO" && (
+                <button name="TO_DO" onClick={onClick}>
+                    To Do
+                </button>
+            )}
+            {category !== "DONE" && (
+                <button name="DONE" onClick={onClick}>
+                    Done
+                </button>
+            )}
         </li>
     )
 }
--- state-management/src/components/atom.tsx
+++ state-management/src/components/atom.tsx
@@ -1,11 +1,11 @@
 import { atom } from "recoil";
 
-export interface ITodo {
+export interface IToDo {
     text: string;
     id: number;
     category: "DONE" | "DOING" | "TO_DO";
 }
-export const toDoState = atom<ITodo[]>({
+export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
 });
