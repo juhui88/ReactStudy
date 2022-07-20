b5878354f001761c5ede39a5440484975de8fcdd
unknown <sand8594@hufs.ac.kr>
2022-07-20 20:10:17 +0900

    scroll animation

--- animations/src/App.tsx
+++ animations/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import { motion, useMotionValue, useTransform, useViewportScroll } from 'framer-motion';
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -12,60 +12,37 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-const Circle = styled(motion.div)`
-  background-color: white;
-  height: 70px;
-  width: 70px;
-  place-self: center;
-  border-radius: 35px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  start: {
-    opacity: 0,
-    scale: 0.5,
-  },
-  end: {
-    scale: 1,
-    opacity: 1,
-    transition: {
-      type: "spring",
-      duration: 0.5,
-      bounce: 0.5,
-      delayChildren: 0.5,
-      staggerChildren: 0.1
-    }
-  }
+  hover: {scale: 1.5, rotateZ: 180},
+  click: {scale: 1, borderRadius: "100px"},
+  drag: {backgroundColor : "rgb(253, 121, 168)", transition: {duration:10 }}
 }
 
-const circleVariants = {
-  start: {
-    opacity: 0,
-    y: 10
-  }, 
-  end : {
-    opacity: 1,
-    y:0,
-  }
-}
 
 function App() {
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x, 
+    [-800, 800],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ])
+    const {scrollYProgress} = useViewportScroll();
+    const scale = useTransform(scrollYProgress, [0,1], [1,5])
   return (
-    <Wrapper>
-      <Box variants={boxVariants} initial="start" animate = "end" >
-        <Circle variants={circleVariants}/>
-        <Circle variants={circleVariants}/>
-        <Circle variants={circleVariants}/>
-        <Circle variants={circleVariants}/>
-      </Box>
-    </Wrapper>
+    <Wrapper style={{background: gradient}}>
+        <Box 
+          style = {{x, rotateZ , scale}}
+          drag = "x"
+          dragSnapToOrigin/>
+     </Wrapper> 
   );
 }
 
